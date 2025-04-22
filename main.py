9c3b0ede1487e607ea123b11db0b4175854ad9c4
SharkSpidy <josephshibuwork@gmail.com>
2025-04-22 18:37:27 +0530

    tweaked a few things!

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
-################################
-wCam, hCam = 640, 480
-################################
+wCam, hCam = 640, 480 #might change it later!
 
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
@@ -22,8 +20,12 @@ devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(
     IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+
 # volume.GetMute()
 # volume.GetMasterVolumeLevel()
+
+
 volRange = volume.GetVolumeRange()
 minVol = volRange[0]
 maxVol = volRange[1]
@@ -36,39 +38,42 @@ colorVol = (255, 0, 0)
 while True:
     success, img = cap.read()
 
-    # Find Hand
+    # basic stufff to Find Hand
     img = detector.findHands(img)
     lmList, bbox = detector.findPosition(img, draw=True)
     if len(lmList) != 0:
 
         # Filter based on size
         area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1]) // 100
+
+
         # print(area)
         if 250 < area < 1000:
 
-            # Find Distance between index and Thumb
+            # to find distance between index and Thumb
             length, img, lineInfo = detector.findDistance(4, 8, img)
-            # print(length)
+
 
             # Convert Volume
             volBar = np.interp(length, [50, 200], [400, 150])
             volPer = np.interp(length, [50, 200], [0, 100])
 
-            # Reduce Resolution to make it smoother
+      
+            # Reduce the resolution to make it smoother
             smoothness = 10
             volPer = smoothness * round(volPer / smoothness)
 
             # Check fingers up
             fingers = detector.fingersUp()
-            # print(fingers)
 
-            # If pinky is down set volume
+
             if not fingers[4]:
                 volume.SetMasterVolumeLevelScalar(volPer / 100, None)
                 cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
                 colorVol = (0, 255, 0)
             else:
                 colorVol = (255, 0, 0)
+
 
     # Drawings
     cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
@@ -79,12 +84,15 @@ while True:
     cv2.putText(img, f'Vol Set: {int(cVol)}', (400, 50), cv2.FONT_HERSHEY_COMPLEX,
                 1, colorVol, 3)
 
-    # Frame rate
+    
+    # Frame rate (Expecting some 30 - 40 ish)
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
     cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,
                 1, (255, 0, 0), 3)
+
+
 
     cv2.imshow("Img", img)
     cv2.waitKey(1)
